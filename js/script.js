daf2e5b86751da0be9a97af85b4dda802e87bee0
Gabriela Ribeiro <103806445+D3vGab@users.noreply.github.com>
2022-06-11 17:11:39 -0400

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,3 +1,4 @@
+/* Slider */
 let count = 1;
 document.getElementById('radio1').checked = true;
 
@@ -12,3 +13,17 @@ function nextImage(){
     };
     document.getElementById('radio'+count).checked = true;
 };
+
+/* Menu */
+var show = true;
+
+var menuHeader = document.querySelector(".menu-header");
+var toggle = menuHeader.querySelector(".toggle");
+
+toggle.addEventListener("click", ()=>{
+
+    document.body.style.overflow = show ? "hidden" : "initial";
+    
+    menuHeader.classList.toggle("on", show);
+    show = !show;
+});
